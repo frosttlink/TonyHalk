656309fedd862a9428a59e74a8f0bbb71d68a8f1
frosttlink <leolorena.alves@gmail.com>
2024-07-30 11:37:07 -0300

    initial commit

---
+++ src/pages/js/menu.js
@@ -0,0 +1,14 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('showing');
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuToggle = document.querySelector('.menu-toggle');
+//     const navUl = document.querySelector('nav ul');
+  
+//     menuToggle.addEventListener('click', function() {
+//       navUl.classList.toggle('showing');
+//     });
+//   });
+  
